904e4a6c28e28496e5a2936eb7cd5b9ec792324c
James Hall <jdhall@live.co.uk>
2021-01-27 05:22:23 +0000

    added view_component_reflex and the application.scss file

---
+++ stim_tail_view.rb
@@ -0,0 +1,13 @@
+# Stimulus Reflex install
+run 'bundle add stimulus_reflex'
+run 'bundle exec rails stimulus_reflex:install'
+
+# tailwindcss
+run 'yarn add tailwindcss postcss autoprefixer'
+run 'mkdir app/javascript/stylesheets'
+run 'curl -L https://raw.githubusercontent.com/directions-needed/rails-templates/main/files/postcss.config.js > app/javascript/stylesheets/application.scss'
+run 'npx tailwindcss init app/javascript/stylesheets/tailwind.config.js'
+run 'curl -L https://raw.githubusercontent.com/directions-needed/rails-templates/main/files/postcss.config.js > postcss.config.js'
+
+# View Component (reflex)
+run 'gem install view_component_reflex'
